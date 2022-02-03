c9bd15ae3bc970ccba12e3d6cde793f52f01d43a
Bulat <87946133+BTARU@users.noreply.github.com>
2022-02-03 19:42:51 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 function slidesPlugin(activeSlide = 0) {
-    const slides = document.querySelectorAll('.slide')
+    let slides = document.querySelectorAll(".slide");
 
-    slides[activeSlide].classList.add('active')
+    slides[activeSlide].classList.add("active");
 
-    for (const slide of slides) {
-        slide.addEventListener('click', () => {
-            clearActiveClasses()
-            
-            slide.classList.add('active')
-        })
+    for (let slide of slides) {
+        slide.addEventListener("click", () => {
+            clearActiveClasses();
+
+            slide.classList.add("active");
+        });
     }
 
     function clearActiveClasses() {
         slides.forEach((slide) => {
-            slide.classList.remove('active')
-        })
+            slide.classList.remove("active");
+        });
     }
 }
 
-slidesPlugin(2)
+slidesPlugin(2);
